ab93c7fc0061a80ea08caf2fbbe8cf7b48e4e9f5
carleeto <carleeto@users.noreply.github.com>
2014-06-24 14:05:24 +1200

    Update graph.go

--- graph.go
+++ graph.go
@@ -9,62 +9,94 @@ const (
 	Int
 )
 
+//Everything needs a name and a description
 type Identifiable interface {
 	Name() string
 	Description() string
 }
 
+//A port also has a type that specifies the type of data it carries
 type Port interface {
 	Identifiable
 	Type() PortType
 }
 
+//An InPort, when used, provides a means of input
+//The Input method takes a certain type.
+//For now, we don't care what that type is
 type InPort interface {
 	Port
 	Input(interface{}) error
 }
 
+//An OutPort, when used, provides a means of output
+//The Output method takes a certain type.
+//For now, we don't care what that type is
 type OutPort interface {
 	Port
 	Output() (interface{}, error)
 }
 
-type Component interface {
-	Identifiable
-	Inputs() []InPort
-	Outputs() []OutPort
+// A source only has outputs
+type Source interface{
+  Outputs() []OutPort
 }
 
-func connect(output, to_input Port) error {
+// A sink only has inputs
+type Sink interface{
+  Inputs() []InPort
+}
+
+//A Link has outputs and inputs
+type Link interface{
+  Identifiable
+  Source
+  Sink
+}
+
+func connect(output OutPort, to_input InPort) error {
 	if output.Type() == to_input.Type() {
 		fmt.Println("connected", output.Name(), "to", to_input.Name())
+    if o, err := output.Output(); err == nil{
+      to_input.Input(o)
+    }
 		return nil
 	}
 	return fmt.Errorf("port types dont match. wont connect")
 }
 
+// Some sample implementations
 type SourceInt struct{}
-
 func (_ SourceInt) Name() string        { return "int source" }
 func (_ SourceInt) Description() string { return "source of integers" }
 func (_ SourceInt) Type() PortType      { return Int }
-func (i SourceInt) Input(interface{}) error {
-	fmt.Println(i.Name(), "input triggered")
-	return nil
-}
-
-type SinkStr struct{}
-
-func (_ SinkStr) Name() string        { return "string sink" }
-func (_ SinkStr) Description() string { return "source of integers" }
-func (_ SinkStr) Type() PortType      { return String }
-func (s SinkStr) Output() (interface{}, error) {
-	fmt.Println(s.Name(), "output triggered")
+func (i SourceInt) Output() (interface{},error) {
+	fmt.Println(i.Name(), "output triggered")
 	return 5, nil
 }
 
+type SinkInt struct{}
+func (_ SinkInt) Name() string        { return "int sink" }
+func (_ SinkInt) Description() string { return "source of integers" }
+func (_ SinkInt) Type() PortType      { return Int }
+func (i SinkInt) Input(d interface{}) error {
+  fmt.Println(i.Name(), "input triggered. data:",d)
+	return nil
+}
+
+//A sink has a least one input
+type SinkStr struct{}
+func (_ SinkStr) Name() string        { return "string sink" }
+func (_ SinkStr) Description() string { return "source of integers" }
+func (_ SinkStr) Type() PortType      { return String }
+func (s SinkStr) Input(interface{}) error {
+	fmt.Println(s.Name(), "output triggered")
+	return nil
+}
+
 func main() {
-	var a1, a2 SourceInt
+	var a1 SourceInt
+  var a2 SinkInt
 	if connect(a1, a2) == nil {
 		println("source and sink port type matches")
 	}
